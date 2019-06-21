c13908aad88e572f180e19d70b173ca3bad09898
Nika AI <49638397+nika-ai@users.noreply.github.com>
2019-06-21 15:21:01 +0300

    IS_LOCAL = true

--- GlobalConfig.ts
+++ GlobalConfig.ts
@@ -1,5 +1,5 @@
 export class GlobalConfig {
-    static readonly IS_LOCAL = false;
-    static readonly DEBUG_MODE = false;
+    static readonly IS_LOCAL = true;
+    static readonly DEBUG_MODE = true;
     static readonly TEST_COM_STUDENT_ID = 110121;
 }
