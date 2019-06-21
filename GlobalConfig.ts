b3939f3cc7014d3684713e42f5bd315c441f74ff
Nika AI <49638397+nika-ai@users.noreply.github.com>
2019-06-21 15:32:30 +0300

    DEV_MODE node env flag is used

--- GlobalConfig.ts
+++ GlobalConfig.ts
@@ -1,5 +1,13 @@
+const DEV_MODE = (() => {
+    try {
+        return process.env.MODE === 'dev';
+    } catch (e) {
+        return false;
+    }
+})();
+
 export class GlobalConfig {
-    static readonly IS_LOCAL = true;
-    static readonly DEBUG_MODE = true;
+    static readonly IS_LOCAL = DEV_MODE || false;
+    static readonly DEBUG_MODE = DEV_MODE || false;
     static readonly TEST_COM_STUDENT_ID = 110121;
 }
