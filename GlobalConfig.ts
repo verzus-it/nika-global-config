395871701f5a49c4418cac2bfd8833d6c1c83f0d
Nika AI <49638397+nika-ai@users.noreply.github.com>
2019-08-02 12:51:07 +0300

    Update GlobalConfig.ts

--- GlobalConfig.ts
+++ GlobalConfig.ts
@@ -7,6 +7,8 @@ const DEV_MODE = (() => {
 })();
 
 export class GlobalConfig {
+    static readonly DEV_MODE = DEV_MODE;
+    static readonly PROD_MODE = !DEV_MODE;
     static readonly IS_LOCAL = DEV_MODE || false;
     static readonly DEBUG_MODE = DEV_MODE || false;
     static readonly TEST_COM_STUDENT_ID = 110121;
